3a11b3ff8b866438462b0465283718a8ebff8371
Thiago Neves <thiagonevesuf@gmail.com>
2017-04-22 11:20:39 -0300

    Limite superior na paginação

--- tp1/src/assets/js/tableFunctions.js
+++ tp1/src/assets/js/tableFunctions.js
@@ -1,13 +1,13 @@
 // NESTE ARQUIVO DEVEM FICAR TODAS AS FUNÇÕES PARA MANIPULAÇÃO DA TABELA
 
 var datafile = "data/dados_tp1.csv";
-var tabledata;
-var tableheader;
+var tabledata; // armazena os dados da tabela (vetor de objetos - criado pelo d3 - ex: tabledata[0].permalink = "blabla", ou tabledata[0][permalink] = "blabla" ;)
+var tableheader; // armazena o cabeçalho da tabela (ex: tableheader[0] = "permalink")
 
 // Parâmetros padrão das funcionalidades
 //paginação
-var page = 0;
-var rowsPerPage = 20;
+var page = 0; // Página atual
+var rowsPerPage = 20; // Quantidade de linhas por página
 // ordenação
 var colSort = 0; // índice da coluna que deve ser ordenada
 var sortDesc = false; // ordenar crescente
@@ -69,17 +69,33 @@ function sortTable(column, desc){
 
 // Configura a paginação de acordo com a pagina e a quantidade de linhas na tabela
 function setPagination(newPage, rows){
+	// limite inferior da paginação
 	if(newPage < 0) newPage = 0;
 	if(rows < 1) rows = rows;
 
+	// limite superior da paginação
+	var maxRows = d3.selectAll("table").selectAll("tbody").selectAll("tr:not(.unmach)").size(); // quantos elementos são visualizaveis
+	var maxPages;
+	if(newPage*rows  >= maxRows){ // se a primeira linha a ser exibida for maior do que a quantidade de linhas visualizaveis
+		maxPages = parseInt(maxRows/rows); // parte inteira da divisão
+		if(maxRows % rows > 0){ // se sobrarem linhas, adiciona uma página
+			maxPages++;
+		}
+		newPage = maxPages -1; // fixa a paginação na última página
+	}
+
+	// atualiza as variáveis globais
 	page = newPage;
 	rowsPerPage = rows;
-	var start = newPage*rowsPerPage; // primeira linha a ser exibida
+
+	// range
+	var start = page*rowsPerPage; // primeira linha a ser exibida
 	var end = start + rowsPerPage -1; // o índice começa de zero
+
 	d3.selectAll("table").selectAll("tbody").selectAll("tr:not(.unmach)")
 		.classed("hidden", function(d, i){
 			return !(start <= i && i <= end) // verdade se i está fora dos limites
-		});
+		}); // esconde as linhas que estiverem fora do range
 
 	d3.selectAll(".page-label").text(page);
 }
@@ -102,5 +118,5 @@ function tableMatch(kw){
 			}
 			return !found;
 		});
-	setPagination(page, rowsPerPage);
+	setPagination(0, rowsPerPage); // volta a paginação para 0
 }
